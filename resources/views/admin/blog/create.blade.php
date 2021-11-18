cf9b61bd52facd1707c928221faef58437b8c285
Kaio Piola <35163593+kaiopiola@users.noreply.github.com>
2021-11-18 20:10:21 -0300

    Correção Issue #5
    Adicionado enctype compatível com envio de arquivos no form.

--- resources/views/admin/blog/create.blade.php
+++ resources/views/admin/blog/create.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <form action="" method="POST">
+                <form action="" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="form-group">
                         <label for="title">title</label>
